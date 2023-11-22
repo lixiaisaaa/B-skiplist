386c3f10e7f9ed60dd7be9f1c45d5df6e05df2fa
Robert Li <1067907540@qq.com>
2023-11-22 03:36:25 -0700

    updated upsert operation

--- BSkipList.cpp
+++ BSkipList.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <limits.h>
 #include <time.h>
+#define B 6
 using namespace std;
 class Block;
 
@@ -12,10 +13,14 @@ class Node
 public:
     int value;
     Block *down; // Pointer to lower level block contains same value
-    Node(int value, Block *down)
+    int opcode;
+    int height;
+    Node(int value, Block *down, int opcode, int height)
     {
         this->value = value;
         this->down = down;
+        this->opcode = opcode;
+        this->height = height;
     }
 };
 
@@ -24,6 +29,7 @@ class Block
 public:
     std::vector<Node *> vector;
     Block *next; // Pointer to the next block at the same level
+    std::vector<Node *> buffer;
     Block(Node *node, Block *next)
     {
         vector.push_back(node);
@@ -31,23 +37,12 @@ public:
         this->next = next;
     }
 
-    Block(std::vector<Node *> vector, Block *next)
+    Block(std::vector<Node *> vector, Block *next, std::vector<Node *> buffer)
     {
         this->vector = vector;
+        this->buffer = buffer;
         // vector.resize(3); // minimum size of each block
         this->next = next;
-    }
-
-    void print()
-    {
-        for (unsigned int i = 0; i < vector.size(); i++)
-        {
-            std::cout << vector[i]->value;
-            if (vector[i]->down)
-                std::cout << "(" << vector[i]->down->vector[0]->value << ")";
-            std::cout << " ";
-        }
-        std::cout << "| ";
     }
 };
 
@@ -57,7 +52,7 @@ private:
     std::vector<Block *> levels; // Vector of head blocks from each level
     std::stack<Block *> getBlockStack(int value)
     {
-        int lvl = levels.size() - 1;
+
         Block *current = levels[levels.size() - 1]; // starting from first block in higest level
         std::stack<Block *> blocks;                 // store the path
         Block *block = current;                     // keep track the place for value
@@ -76,7 +71,6 @@ private:
                 { // find the place
                     blocks.push((block));
                     current = prev->down;
-                    lvl--;
                     block = current;
                     found = true;
                     break;
@@ -107,14 +101,20 @@ private:
 
 public:
     int r = 1;
-    // const int MAX_LEVEL = 32;
-    // const float P_FACTOR = 0.25;
+
+    const int MAX_LEVEL = 32;
+    const float P_FACTOR = 0.25;
+    static constexpr int P = 4;
+    static constexpr int S = 0xFFFF;
+    static constexpr int PS = S / 4;
+
+
     // static std::random_device rd; // obtain a random number from hardware
     // static std::mt19937 gen(rand()); // seed the generator
     // static std::uniform_real_distribution<> distr(0, 1); // define the range
     BSkipList()
     {
-        Block *block = new Block(new Node(INT_MIN, nullptr), nullptr); // negative infinity block
+        Block *block = new Block(new Node(INT_MIN, nullptr, 0, -1), nullptr); // negative infinity block
         levels.push_back(block);
     }
 
@@ -123,77 +123,126 @@ public:
         // Destructor to free memory
         // ... (cleanup logic here)
     }
+    int randomLevel() {
+        int lv = 1;
+        
+        while (lv < MAX_LEVEL && (rand() & S) < PS) ++lv;
+        return lv;
+    }
+
+    void upsert(int value, int opcode,int lv)
+    {
+        
+        //levels[levels.size()-1]->buffer.push_back(new Node(value, nullptr, opcode, lv));
+        //如果层数比当前最高lvl高的情况
+        if(lv >= levels.size()){
+            //向上
+            levels[levels.size()-1]->next = new Block(new Node(value, nullptr, 0, lv), nullptr);
+            for(int i = levels.size(); i <= lv; i++){
+                
+                if(i==lv){
+                    levels.push_back(new Block(new Node(INT_MIN, nullptr, 0, -1), nullptr));
+                    levels[i]->vector.push_back(new Node(value, nullptr, 0, lv));
+                }else{
+                    levels.push_back(new Block(new Node(INT_MIN, nullptr, 0, -1), nullptr));
+                    levels[i]->next = new Block(new Node(value, nullptr, 0, lv), nullptr);
+
+                }
+            }
+            //向下
+            int sz = levels[levels.size()-1]->vector.size();
+            Node* cur = levels[levels.size()-1]->vector[sz-1];
+            while(cur->down){
+                
+            }
+        }else{
+            levels[levels.size()-1]->buffer.push_back(new Node(value, nullptr, opcode, lv));
+        }
+
+        
+        
+        
+        
+
+        // for (int i = levels.size() - 1; i >= 0; i--)
+        // {
+        //     Block* curr = levels[i];
+        // }
+    }
 
     void insert(int value)
     {
-        srand(time(NULL)); // initialize random seed
-        std::stack<Block *> blocks = getBlockStack(value);
-        Block *lower = nullptr;
-        // building block from botton
-        while (!blocks.empty())
-        {
-            bool inserted = false;
-            Block *block = blocks.top();
-            blocks.pop();
-            for (unsigned int i = 0; i < block->vector.size(); i++)
-            {
-                if (block->vector[i]->value > value)
-                { // in the middle of the vector
-                    if (r % 2 == 0)
-                    { // tail
-                        r = r + rand();
-                        block->vector.insert(block->vector.begin() + i, new Node(value, lower));
-                        return;
-                    }
-                    else
-                    { // head
-                        r++;
-                        // split and shrink block
-                        std::vector<Node *> right;
-                        right.push_back(new Node(value, lower));
-                        for (unsigned int j = i; j < block->vector.size(); j++)
-                            right.push_back(block->vector[j]);
-                        block->vector.resize(i);
-                        Block *rightBlock = new Block(right, block->next);
-                        block->next = rightBlock;
-                        // new level
-                        if (blocks.empty())
-                        {
-                            Block *up = new Block(new Node(INT_MIN, block), nullptr);
-                            up->vector.push_back(new Node(value, block->next));
-                            levels.push_back(up);
-                        }
-                        inserted = true;
-                        lower = block->next;
-                        break;
-                    }
-                }
-            }
-            if (!inserted)
-            {
-                // at the end of the vector
-                if (r % 2 == 0)
-                { // tail
-                    r = r + 1;
-                    block->vector.push_back(new Node(value, lower));
-                    return;
-                }
-                else
-                { // head
-                    r = r + rand();
-                    Block *newBlock = new Block(new Node(value, lower), block->next);
-                    block->next = newBlock;
-                    // new level
-                    if (blocks.empty())
-                    {
-                        Block *up = new Block(new Node(INT_MIN, block), nullptr);
-                        up->vector.push_back(new Node(value, newBlock));
-                        levels.push_back(up);
-                    }
-                    lower = newBlock;
-                }
-            }
-        }
+        // upsert(value, 0);
+        //  srand(time(NULL)); // initialize random seed
+        //  std::stack<Block *> blocks = getBlockStack(value);
+        //  Block *lower = nullptr;
+        //  // building block from botton
+        //  while (!blocks.empty())
+        //  {
+        //      bool inserted = false;
+        //      Block *block = blocks.top();
+        //      blocks.pop();
+        //      for (unsigned int i = 0; i < block->vector.size(); i++)
+        //      {
+        //          if (block->vector[i]->value > value)
+        //          { // in the middle of the vector
+        //              // (static_cast<float>(rand()) / RAND_MAX) < P_FACTOR)
+        //              if (r%2==0)
+        //              { // tail
+        //                  //r = r + rand();
+        //                  block->vector.insert(block->vector.begin() + i, new Node(value, lower));
+        //                  return;
+        //              }
+        //              else
+        //              { // head
+        //                  r++;
+        //                  // split and shrink block
+        //                  std::vector<Node *> right;
+        //                  std::vector<Node *> buffer;
+        //                  right.push_back(new Node(value, lower));
+        //                  for (unsigned int j = i; j < block->vector.size(); j++)
+        //                      right.push_back(block->vector[j]);
+        //                  block->vector.resize(i);
+        //                  Block *rightBlock = new Block(right, block->next, buffer);
+        //                  block->next = rightBlock;
+        //                  // new level
+        //                  if (blocks.empty())
+        //                  {
+        //                      Block *up = new Block(new Node(INT_MIN, block), nullptr);
+        //                      up->vector.push_back(new Node(value, block->next));
+        //                      levels.push_back(up);
+        //                  }
+        //                  inserted = true;
+        //                  lower = block->next;
+        //                  break;
+        //              }
+        //          }
+        //      }
+        //      if (!inserted)
+        //      {
+        //          // at the end of the vector
+        //          if (r%2==0)
+        //          { // tail
+        //              r = r + 1;
+        //              block->vector.push_back(new Node(value, lower));
+        //              return;
+        //          }
+        //          else
+        //          { // head
+        //              r = r + rand();
+        //              Block *newBlock = new Block(new Node(value, lower), block->next);
+        //              block->next = newBlock;
+        //              // new level
+        //              if (blocks.empty())
+        //              {
+        //                  Block *up = new Block(new Node(INT_MIN, block), nullptr);
+        //                  up->vector.push_back(new Node(value, newBlock));
+        //                  levels.push_back(up);
+        //              }
+        //              lower = newBlock;
+        //          }
+        //      }
+        //  }
     }
 
     void remove(int value)
@@ -218,7 +267,7 @@ public:
                         {
                             flag = true;
                             update.push_back(pre);
-                            //cout << pre->vector[0]->value << "pre" << endl;
+                            // cout << pre->vector[0]->value << "pre" << endl;
                         }
                         break;
                     }
@@ -262,11 +311,14 @@ public:
                     {
                         current = downBlock->vector[0]->down;
                         downBlock->vector.erase(downBlock->vector.begin());
-                        if(!downBlock->vector.empty()){
-                            update[x]->vector.insert(update[x]->vector.end(), downBlock->vector.begin(),downBlock->vector.end());
+                        if (!downBlock->vector.empty())
+                        {
+                            update[x]->vector.insert(update[x]->vector.end(), downBlock->vector.begin(), downBlock->vector.end());
                             update[x]->next = update[x]->next->next;
                             x++;
-                        }else{
+                        }
+                        else
+                        {
                             update[x]->next = update[x]->next->next;
                             x++;
                         }
@@ -278,8 +330,9 @@ public:
         }
     }
 
-    void print_list(){
-        Block* curr;
+    void print_list()
+    {
+        Block *curr;
         for (int i = levels.size() - 1; i >= 0; i--)
         {
             Block *pre = nullptr;
@@ -288,27 +341,22 @@ public:
             {
                 for (int j = 0; j < curr->vector.size(); j++)
                 {
-                    
+
                     cout << curr->vector[j]->value << " ";
+                    if (curr->vector[j]->down)
+                    {
+                        cout << "(" << curr->vector[j]->down->vector[0]->value << ")";
+                    }
+                    
+                }
+
+                for(int k = 0; k < curr->buffer.size();k++){
+                    cout << "["<<curr->buffer[k]->value << "]";
                 }
                 curr = curr->next;
                 cout << "|";
             }
-            cout << " " << endl;
-        }
-    }
-
-    void print()
-    {
-        for (unsigned int i = levels.size() - 1; i >= 0; i--)
-        {
-            Block *current = levels[i];
-            while (current)
-            {
-                current->print();
-                current = current->next;
-            }
-            std::cout << std::endl;
+            cout << "" << endl;
         }
     }
 
@@ -318,8 +366,6 @@ public:
         Node *node;
         Node *prev_node;
         Block *block = levels[levels.size() - 1];
-
-        
 
         while (block)
         {
@@ -370,55 +416,25 @@ public:
     }
 };
 
-void test_search(BSkipList list)
-{
-    // Test Search
-    std::cout << "==========================" << std::endl;
-    std::cout << "Test for search" << std::endl;
-    std::cout << "==========================" << std::endl;
-    std::cout << "Search 1: " << std::boolalpha << list.search(1) << std::endl;
-    std::cout << "Search 3: " << std::boolalpha << list.search(3) << std::endl;
-    std::cout << "Search 4: " << std::boolalpha << list.search(4) << std::endl;
-    std::cout << "Search 10: " << std::boolalpha << list.search(10) << std::endl;
-    std::cout << "Search 5: " << std::boolalpha << list.search(5) << std::endl;
-    std::cout << "Search 14: " << std::boolalpha << list.search(5) << std::endl;
-    std::cout << "Search 2: " << std::boolalpha << list.search(5) << std::endl;
-}
-
-void test_range_query(BSkipList list, int start, int end)
-{
-    // Test Range Query
-    std::vector<bool> rq_output = list.range_query(start, end);
-    std::vector<bool>::iterator it;
-    int i;
-
-    std::cout << "==========================" << std::endl;
-    std::cout << "Test for range search from " << start << " to " << end << std::endl;
-    std::cout << "==========================" << std::endl;
-
-    for (it = rq_output.begin(), i = start; it != rq_output.end() && i < end; it++, i++)
-    {
-        std::cout << "Search " << i << ": " << std::boolalpha << *it << std::endl;
-    }
-}
-
 int main()
 {
     BSkipList list;
-    list.insert(1);
-    list.insert(10);
-    list.insert(3);
-    list.insert(2);
-    list.insert(6);
-    list.insert(11);
-    list.insert(7);
-    list.remove(7);
-    list.print_list();
+    list.upsert(1,0,3);
+    list.upsert(2,0,3);
+    // list.insert(10);
+    // list.insert(3);
+    // list.insert(2);
+    // list.insert(6);
+    // list.insert(11);
+    // list.insert(7);
+    // list.print_list();
+    // list.remove(7);
+    
     // list.insert(8);
     // list.insert(-1);
     // std::cout << list.search(-1) << std::endl;
     // std::cout << list.search(-2) << std::endl;
     // std::cout << list.search(11) << std::endl;
-    //list.print();
+    list.print_list();
     return 0;
 }
